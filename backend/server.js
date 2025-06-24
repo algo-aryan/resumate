6539a93adb73a931962f846f8e57e485271dff39
Aryan Bansal <aryan1509bansal@gmail.com>
2025-06-24 15:45:41 +0530

    fix server

--- backend/server.js
+++ backend/server.js
@@ -337,4 +337,4 @@ app.use('/api', atsScoreRoute);
 
 // ✅ Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
